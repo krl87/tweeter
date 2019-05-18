49a07200456648d565dc897eed5af9f07dc44fc4
Kayley <kayley.luftig@gmail.com>
2019-05-18 22:42:49 +0000

    retrieve and create tweets with mongo

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -8,20 +8,28 @@ module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+    saveTweet: function (newTweet, callback) {
+
+        db.collection("tweets").insert(newTweet);
+
         callback(null, true);
-      });
+
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
-    }
+      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
 
-  };
+      db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets.sort(sortNewestFirst));
+    });
+    },
+  }
+
+    //     callback(null, db.tweets.sort(sortNewestFirst));
+    // }
+
 }
