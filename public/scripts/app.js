cedc48eebb009aa8322eb6e94bd20aa4131aa38e
Kayley <kayley.luftig@gmail.com>
2019-05-19 18:38:15 +0000

    create helper function to convert ms to readable time

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,49 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+
+// Client-side JS logic goes here
+
+//time helper function
+function convertTime(time1, time2) {
+  const msMin = 60 * 1000;
+  const msHour = msMin * 60;
+  const msDay = msHour * 24;
+  const msMonth = msDay * 30;
+  const msYear = msDay * 365;
+
+  let timeDiff = time1 - time2;
+
+  if (timeDiff < msMin) {
+    return "Just now";
+  } else if (timeDiff < msHour) {
+    return Math.round(timeDiff / msMin) + " minutes ago";
+  } else if (timeDiff < msDay) {
+    return Math.round(timeDiff / msHour) + " hours ago";
+  } else if (timeDiff < msMonth) {
+    return Math.round(timeDiff / msDay) + " days ago";
+  } else if (timeDiff < msYear) {
+    return Math.round(timeDiff / msMonth) + " months ago";
+  } else {
+    return Math.round(timeDiff / msYear) + " years ago";
+  }
+}
+
 
 // create html via jquery
 function createTweetElement(tweet) {
-
- // return `<header>
- // <h1>${tweet.user.name}</h1>
- // `
- let $header = $("<header>").append($("<img>").attr("src", tweet.user.avatars.small));
- $header.append($("<h1>").text(tweet.user.name));
- $header.append($("<h2>").text(tweet.user.handle));
- let $content = $("<div>").text(tweet.content.text);
- let $footer = $("<footer>");
- $footer.append("<div>").text(tweet.created_at);
- let $iconDiv = $("<div>");
- $iconDiv.append($("<i>").addClass("fas fa-flag font-awesome"));
- $iconDiv.append($("<i>").addClass("fas fa-retweet font-awesome"));
- $iconDiv.append($("<i>").addClass("fas fa-heart font-awesome"));
- $footer.append($iconDiv);
- let $tweet = $("<article>").addClass("tweet");
- $tweet.append($header).append($content).append($footer);
- return $tweet;
+  const tweetTime = convertTime(Date.now(), tweet.created_at);
+  let $header = $("<header>").append($("<img>").attr("src", tweet.user.avatars.small));
+  $header.append($("<h1>").text(tweet.user.name));
+  $header.append($("<h2>").text(tweet.user.handle));
+  let $content = $("<div>").text(tweet.content.text);
+  let $footer = $("<footer>");
+  $footer.append("<div>").text(tweetTime);
+  let $iconDiv = $("<div>");
+  $iconDiv.append($("<i>").addClass("fas fa-flag font-awesome"));
+  $iconDiv.append($("<i>").addClass("fas fa-retweet font-awesome"));
+  $iconDiv.append($("<i>").addClass("fas fa-heart font-awesome"));
+  $footer.append($iconDiv);
+  let $tweet = $("<article>").addClass("tweet");
+  $tweet.append($header).append($content).append($footer);
+  return $tweet;
 }
 
 //loop through tweet json and populate tweets
@@ -39,13 +59,11 @@ function renderTweets(tweets) {
 function ajaxRequest() {
   $("form").on("submit", function(e){
     e.preventDefault();
-
   if ($("textarea").val() === "") {
-    // alert("Please enter a tweet");
     $(".error1").slideDown();
   } else if ($("textarea").val().length > 140 ) {
-    $(".error2").slideDown();
-  } else  {
+      $(".error2").slideDown();
+  } else {
       $.ajax({
         type: "POST",
         url: "/tweets",
@@ -54,6 +72,7 @@ function ajaxRequest() {
       .done(function(){
         loadTweets();
       })
+    $("textarea").val("");
     }
   });
 }
@@ -67,8 +86,6 @@ function loadTweets() {
     renderTweets(data);
   });
 }
-
-
 
 //fire functions
 $(document).ready(function() {
